4ff252ac063af99e9d442e6106dff07d09e7b227
psunseries <pea.chiwa@sunseries.travel>
2019-05-27 13:23:20 +0700

    Added Swagger into project

--- src/main/java/com/scb/bookstore/controller/UserController.java
+++ src/main/java/com/scb/bookstore/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.scb.bookstore.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +15,13 @@ import javax.servlet.http.HttpServletRequest;
 @Api(value="User", description="Api for manager users.")
 public class UserController {
 
-    @GetMapping(value = "/hello")
+    @ApiOperation(value = "Welcome API.", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Welcome user!")
+    })
+    @GetMapping(value = "/welcome")
     public String hello(HttpServletRequest req){
-        return "hello";
+        return "Welcome user!";
     }
 
 }
